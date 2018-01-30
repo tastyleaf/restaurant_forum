c3f5566e95797e2c5c7812f47bd1ca2c76d932f9
Unknown <thetasteofleavesisgreat@gmail.com>
2018-01-30 15:50:22 +0800

    add favorite / unfavorite action on RestaurantsController

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,4 @@
 class Favorite < ApplicationRecord
   belongs_to :user
-  has_many :favorites, dependent: :destroy
-  has_many :favorited_users, through: :favorites, source: :user  
+  belongs_to :restaurant 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,5 +3,6 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name
   belongs_to :category
   has_many :comments, dependent: :destroy
-  has_many :fac
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
 end
