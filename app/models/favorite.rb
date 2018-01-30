c509d90eb0bfee0fd244db5eaeebea6f993363dd
Unknown <thetasteofleavesisgreat@gmail.com>
2018-01-30 14:58:17 +0800

    setup relation among user, restaurant and favorite

---
+++ app/models/favorite.rb
@@ -0,0 +1,5 @@
+class Favorite < ApplicationRecord
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user  
+end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,5 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name
   belongs_to :category
   has_many :comments, dependent: :destroy
+  has_many :fac
 end
